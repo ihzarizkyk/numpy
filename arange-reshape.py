d0cf8ad17f2232c4b2a3ed83dee9a0b881762c1b
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-05-27 18:33:42 +0700

    Create arange-reshape.py

---
+++ arange-reshape.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+d = np.arange(16).reshape(4,4)
+
+print(d)
+
+'''
+Output :
+
+[[ 0  1  2  3]
+ [ 4  5  6  7]
+ [ 8  9 10 11]
+ [12 13 14 15]]
+
+'''
+
+e = np.arange(12)
+
+print(e)
+
+'''
+Output : 
+
+[ 0  1  2  3  4  5  6  7  8  9 10 11]
+
+'''
+
