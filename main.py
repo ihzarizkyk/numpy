2b1c16a7765dbab0c85499532e6998b4c28744f2
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-05-27 18:20:51 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import numpy as np 
+
+a = np.array([1,2,3])
+b = np.array([1,2,3])
+
+print(a)
+print(b)
+print(a+b)
+print(a-b)
+print(a*b)
+
+'''
+Output a : [1 2 3]
+Output b : [1 2 3]
+
+Output Penjumlahan : 
+
+[2 4 6]
+
+Output Pengurangan  :
+
+
+[0 0 0]
+
+
+Output Perkalian : 
+
+[1 4 9]
+ 
+'''
