062c96732c419333cb3a18d69c2c486bd6df4478
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-05-30 06:06:24 +0700

    Create transpose-matriks.py

---
+++ transpose-matriks.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+matriksC = np.array([[1,2,3],
+					 [4,5,6]])
+
+print("Matriks C : ")
+print(matriksC)
+
+transpose_matriks = np.transpose(matriksC)
+
+print("Tranpose Matriks C : ")
+print(transpose_matriks)
+
+'''
+Output :
+
+Matriks C : 
+[[1 2 3]
+ [4 5 6]]
+ 
+Tranpose Matriks C : 
+[[1 4]
+ [2 5]
+ [3 6]]
+ 
+'''
