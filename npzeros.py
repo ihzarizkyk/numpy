44dbcfa7cf1aac4677e5ffc9d12894c4d174525d
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-06-03 16:22:00 +0700

    Create npzeros.py

---
+++ npzeros.py
@@ -0,0 +1,19 @@
+'''
+Author : Mochammad Ihza Rizky Karim
+'''
+import numpy as np
+
+# buat array 1 dimensi 
+b = np.array([1,2])
+
+# buat variabel zeros yang menampung fungsi np.zeros yang didalamnya terdapat parameter variabel b
+zeros = np.zeros(b)
+
+#terakhir print variabel zeros
+print(zeros)
+
+'''
+Output : 
+[[0.0]]
+  
+'''
